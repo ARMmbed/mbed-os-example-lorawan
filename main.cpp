ada8ae60f4281490e1742e776fc70cdb9b56b480
Kimmo Vaisanen <kimmo.vaisanen@arm.com>
2018-01-08 09:40:09 +0200

    Add stdio.h for printf

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <stdio.h>
 #include "lorawan/LoRaWANInterface.h"
 #include "lorawan/system/lorawan_data_structures.h"
 #include "events/EventQueue.h"
